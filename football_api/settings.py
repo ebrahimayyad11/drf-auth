e55bfceec6183a919a69765c606b62fba24e5c4b
ebrahim ayyad <ayyad_ebrahim@hotmail.com>
2021-07-28 16:44:22 +0300

    doing lab-33

--- football_api/settings.py
+++ football_api/settings.py
@@ -44,8 +44,14 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
 }
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
